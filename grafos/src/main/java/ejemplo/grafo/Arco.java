c547e077e69dbf97afd3c5b5f1aa8b6c658d4135
jprietouniandes <47231655+jprietouniandes@users.noreply.github.com>
2019-09-27 15:25:16 -0500

    Taller de grafos
    Se hizo sobre el código base proporcionado, se opto por eliminar el paquete que trae el peso del arco y se agrega el peso del arco por defecto en todos los proyectos, teniendo en cuenta que si no hay peso para el grafo se coloca un cero para la creación de los arcos.

---
+++ grafos/src/main/java/ejemplo/grafo/Arco.java
@@ -0,0 +1,33 @@
+package main.java.ejemplo.grafo;
+
+
+public class Arco {
+
+	private Nodo 	origen;
+	private Nodo 	destino;
+	private int		peso;
+	
+	public Arco() {	}
+	
+	public Nodo getOrigen() {
+		return origen;
+	}
+	public void setOrigen(Nodo origen) {
+		this.origen = origen;
+	}
+	
+	public Nodo getDestino() {
+		return destino;
+	}
+	public void setDestino(Nodo destino) {
+		this.destino = destino;
+	}
+	
+	public int getPeso() {
+		return peso;
+	}
+	public void setPeso(int peso) {
+		this.peso = peso;
+	}
+	
+}
